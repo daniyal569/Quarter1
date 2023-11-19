7329011c7c0de5d1f03c1183db25109a567a7db7
Daniyal khalil <daniyalkhalil569@gmail.com>
2023-11-19 05:55:01 -0800

    Just Practicing Typescript

---
+++ TS-JS/index.ts
@@ -0,0 +1,54 @@
+console.log("Hello World")
+
+//LOGICAL OPERATORS
+
+console.log(123456789)
+let x= 53 
+let y=223
+console.log("Arithmetic Operations with x and y variables")
+console.log("sum of x+y is equal", x+y)
+console.log("y-x is equal", y-x)
+console.log("x multiplied by y is equal", x*y)
+console.log("x/y is equal", x/y)
+console.log("y raised to power x is equal", y**x)
+console.log("y%x is equal to", y%x)
+
+//Backstick method to print output without concosion
+
+let firstname: string = "Daniyal"
+let lastname: string = "Khalil"
+let institute: string = "PANAVERSE"
+let batchno: number = 52
+let quarter: number = 1
+
+console.log(`My name is ${firstname} ${lastname} Institute I am studying Web 3.0 development is ${institute} 
+My batch number is ${batchno} and Quarter number is ${quarter}` )
+
+//ASSIGNMENT OPERATORS
+
+let a = 69
+a = 65
+a = 34
+
+a+= 26
+a-= 70
+console.log(a)
+
+//Comparison Operators
+
+let n=20
+console.log(n==19)
+console.log(n===20)
+console.log(n<=20)
+console.log(n<=30)
+console.log(n>=15)
+console.log(n<10)
+console.log(n>=20)
+console.log(n!=20)
+console.log(n!=69)
+
+//Logical Operators
+
+console.log(n<3 && n<21) //F T F
+console.log(n!=50 && n>=20)//T T T
+console.log(n!=20 || n>=10)//F T T
